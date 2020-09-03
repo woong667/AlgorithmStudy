ea463a0a190787fdd9dac6ae4d0b8e5576edfa28
woong667 <woong667@naver.com>
2020-09-03 18:13:49 +0900

    0903 수정

--- Backjoon/BOJ 1987(알파벳)-백트래킹..ㅠ어려워.cpp
+++ Backjoon/BOJ 1987(알파벳)-백트래킹..ㅠ어려워.cpp
@@ -1,79 +1,58 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <vector>
-#include <string>
+#include <functional>
+#include <algorithm>
 using namespace std;
-//A는 아스키로 65
-//ifstream input("a.inp");
-//ofstream output("a.out");
-char arr[20][20];
-string a[20];
-int R, C;
-bool eng[26];
-int get_x[4] = { -1,0,1,0 };       //좌->상->우->하
-int get_y[4] = { 0,-1,0,1 };       //좌->상->우->하
+int arr[21][21];             
+bool arr_check[21][21];      //들른장소인지
+bool check[26];              //사용된 알파벳인지.
+int mx[4] = { 0,-1,0,1 };
+int my[4] = { -1,0,1,0 };
+int N, M;
 int MAX = 0;
-int cnt= 0;
 
-bool full()
-{
-	for (int i = 0; i < 26; i++)
+void dfs(int x, int y,int sum) {
+	
+	if (sum > MAX)
+		MAX = sum; //최댓값 갱신
+	int nx,ny;
+	for (int i = 0; i < 4; i++)
 	{
-		if (eng[i] == false)
-			return false;
-	}
-	return true;  //
-}
+		nx = x + mx[i];
+		ny = y + my[i];
+		if (nx<0 || ny<0 || nx>N - 1 || ny>M - 1)
+			continue;
 
-bool go(int i, int j)
-{
-	if (full())   //full이면 더이상 갈 수 있는곳없다는 뜻.
-		return false;
-
-	if (eng[arr[i][j] - 65] == true)
-		return false;
-	else
-	{
-		eng[arr[i][j] - 65] = true;
-		cnt++;
-		if (cnt > MAX)
-			MAX = cnt;
-		for (int k = 0; k < 4; k++)
-	    {
-			if (j + get_x[k]<0 || j + get_x[k]>C - 1 || i + get_y[k]<0 || i + get_y[k]>R - 1||eng[arr[i+get_y[k]][j+get_x[k]]-65]==true)
-			{
-				continue;
-			} //돌아가야한다.
-		    else
-		    {
-				go(i + get_y[k], j + get_x[k]);
-		    }
+		if (arr_check[nx][ny] == false && check[arr[nx][ny]] == false) //아직 들르지 않은곳이면서 아직 밟지 않은 알파벳이라면.
+		{
+			arr_check[nx][ny] = true;
+			check[arr[nx][ny]] = true; //들렀던 알파벳 처리해주고
+			dfs(nx, ny, sum + 1);
+			arr_check[nx][ny] = false;
+			check[arr[nx][ny]] = false; //나왔으니까 빼준다.
 		}
-		eng[arr[i][j] - 65] = false;
-		cnt--;
-		return false;
 	}
-	return true;
 }
+
 int main()
 {
-	cin >> R >> C;
-	for (int i = 0; i < R; i++)
+	ifstream input("a.inp");
+	input >> N >> M;
+	char inp;
+	for (int i = 0; i < N; i++)
 	{
-		cin >> a[i];
-	}
-	for (int i = 0; i < R; i++)
-	{
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < M; j++)
 		{
-			arr[i][j] = a[i][j];
+			input >> inp;
+			arr[i][j] = inp - 65;  //숫자로 넣어준다.
 		}
 	}
-	go(0, 0);
-	cout << MAX << endl;
-	//input.close();
-	//output.close();
+	arr_check[0][0] = true;
+	check[arr[0][0]] = true;
+	dfs(0,0,1);
+	cout << MAX;
+	input.close();
 	return 0;
-	
+
 }
