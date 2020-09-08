47036c797a3a7830c61333570d37742bc6db58cf
KUN WOONG <66250847+woong667@users.noreply.github.com>
2020-09-08 16:59:44 +0900

    Update BOJ 1987(알파벳)-백트래킹.cpp

--- Backjoon/BOJ 1987(알파벳)-백트래킹.cpp
+++ Backjoon/BOJ 1987(알파벳)-백트래킹.cpp
@@ -1,84 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <fstream>
+#include <vector>
+#include <functional>
 #include <algorithm>
 using namespace std;
-int arr[100001];
-int L, N;
+int arr[21][21];             //입력받는 배열
+bool arr_check[21][21];      //들른장소인지 check할 배열
+bool check[26];              //사용된 알파벳인지 check할 배열
+int mx[4] = { 0,-1,0,1 };   
+int my[4] = { -1,0,1,0 };    //움직이는 
+int N, M;
+int MAX = 0;
 
-int method1(int idx)
-{
-	int cnt = 1;
-	int bef = idx - 1, aft = idx + 1;
-	while (bef>0)   //0만 되더라도 진행할 필요없음
+void dfs(int x, int y,int sum) {
+	
+	if (sum > MAX)
+		MAX = sum;  //최댓값 갱신
+	int nx,ny;
+	for (int i = 0; i < 4; i++)
 	{
-		bef -= N - 1;
-		cnt++;
-	}
-	while (aft < L - 1)  //끝에 도달하기 전까지
-	{
-		aft += N - 1;
-		cnt++;
-	}
-	return cnt;
-}
+		nx = x + mx[i];
+		ny = y + my[i];
+		if (nx<0 || ny<0 || nx>N - 1 || ny>M - 1)
+			continue;     //범위를 나가면 CONTINUE;
 
-int method2(int idx)
-{
-	int cnt = 1;
-	int bef = idx, aft = idx + 2;
-	while (bef > 0)   //0만 되더라도 진행할 필요없음
-	{
-		bef -= N - 1;
-		cnt++;
+		if (arr_check[nx][ny] == false && check[arr[nx][ny]] == false) //아직 들르지 않은곳이면서 아직 밟지 않은 알파벳이라면.
+		{
+			arr_check[nx][ny] = true;
+			check[arr[nx][ny]] = true; //들렀던 알파벳 처리해주고
+			dfs(nx, ny, sum + 1);
+			arr_check[nx][ny] = false;  
+			check[arr[nx][ny]] = false; //나왔으니까 빼준다.
+		}
 	}
-	while (aft < L - 1)  //끝에 도달하기 전까지
-	{
-		aft += N - 1;
-		cnt++;
-	}
-	return cnt;
-}
-
-int method3(int idx)
-{
-	int cnt = 1;
-	int bef = idx-2, aft = idx;
-	while (bef > 0)   //0만 되더라도 진행할 필요없음
-	{
-		bef -= N - 1;
-		cnt++;
-	}
-	while (aft < L - 1)  //끝에 도달하기 전까지
-	{
-		aft += N - 1;
-		cnt++;
-	}
-	return cnt;
 }
 
 int main()
 {
-	ifstream input("a.inp");
-	int idx,MIN=100001;
-	int cnt = 1000001;
-	input >> L >> N;
-	for (int i = 0; i < L; i++)
+	//ifstream input("a.inp");
+	cin >> N >> M;
+	char inp;
+	for (int i = 0; i < N; i++)
 	{
-		input >> arr[i];
-		if (arr[i] < MIN)
+		for (int j = 0; j < M; j++)
 		{
-			MIN = arr[i];
-			idx = i;
-		} //최소 위치를 초기화.
+			cin >> inp;
+			arr[i][j] = inp - 65;  //숫자로 넣어준다. 아스키로 해결하기위해 A=0으로 해줬다
+		}
 	}
-	int a=method1(idx);
-	int b = method2(idx);
-	int c = method3(idx);
-	cout << min(a, min(b, c))<<endl;
-	input.close();
+	arr_check[0][0] = true; //첫위치는 들렀고
+	check[arr[0][0]] = true; //첫위치의 알파벳은 사용됫다
+	dfs(0,0,1);              //0,0 위치에서 CNT=1로 DFS돌린다.
+	cout << MAX;
+	//input.close();
 	return 0;
-
 
 }
